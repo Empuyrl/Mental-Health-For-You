da2babe3ed5f735bee2bb9d14e710ece8adf005c
User <user@Users-MacBook-Pro.local>
2023-06-19 16:36:42 -0500

    router path on server and the get requests for each mental area updated, just need to test

--- server/routes/mentalchallenges.router.js
+++ server/routes/mentalchallenges.router.js
@@ -1,30 +1,67 @@
 const express = require('express');
 const router = express.Router();
 
-// Route for retrieving depression-related data
+// GET route to fetch depression response
 router.get('/depression', (req, res) => {
-  // Handle retrieving depression data
-});
+    // Perform a database query to retrieve the depression response
+    const queryText = 'SELECT * FROM depression_response';
+    pool
+      .query(queryText)
+      .then((result) => {
+        const response = result.rows[0];
+        res.send(response);
+      })
+      .catch((error) => {
+        console.error('Error fetching depression response:', error);
+        res.sendStatus(500);
+      });
+  });
+  // Route for submitting depression questionnaire responses
+  router.post('/depression/response', (req, res) => {
+    // Handle submitting depression questionnaire responses
+    // Example:
+    const { answers } = req.body;
+    // Process the submitted answers and save them to the database
+    res.sendStatus(200);
+  });
+  
 
-// Route for submitting depression questionnaire responses
-router.post('/depression/response', (req, res) => {
-  // Handle submitting depression questionnaire responses
-});
-
-// Route for retrieving anxiety-related data
+// GET route to fetch anxiety response
 router.get('/anxiety', (req, res) => {
-  // Handle retrieving anxiety data
-});
+    // Perform a database query to retrieve the anxiety response
+    const queryText = 'SELECT * FROM anxiety_response';
+    pool
+      .query(queryText)
+      .then((result) => {
+        const response = result.rows[0];
+        res.send(response);
+      })
+      .catch((error) => {
+        console.error('Error fetching anxiety response:', error);
+        res.sendStatus(500);
+      });
+  });
 
 // Route for submitting anxiety questionnaire responses
 router.post('/anxiety/response', (req, res) => {
   // Handle submitting anxiety questionnaire responses
 });
 
-// Route for retrieving stress-related data
+// GET route to fetch stress response
 router.get('/stress', (req, res) => {
-  // Handle retrieving stress data
-});
+    // Perform a database query to retrieve the stress response
+    const queryText = 'SELECT * FROM stress_response';
+    pool
+      .query(queryText)
+      .then((result) => {
+        const response = result.rows[0];
+        res.send(response);
+      })
+      .catch((error) => {
+        console.error('Error fetching stress response:', error);
+        res.sendStatus(500);
+      });
+  });
 
 // Route for submitting stress questionnaire responses
 router.post('/stress/response', (req, res) => {
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const mentalRouter = require('./routes/mentalchallenges.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
