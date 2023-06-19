b9749877e220775d12b639f1befd03566cdd38ce
User <user@Users-MacBook-Pro.local>
2023-06-19 11:07:12 -0500

    installed, setup database and changed the pool

--- database.sql
+++ database.sql
@@ -6,5 +6,30 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) 
+);
+
+CREATE TABLE "entries" (
+  "id" SERIAL PRIMARY KEY,
+  "entry_text" TEXT NOT NULL,
+  "rating" INTEGER NOT NULL,
+  "user_id" INTEGER REFERENCES "user",
+  "createdate" DATE NOT NULL
+);
+
+CREATE TABLE "response" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" INTEGER REFERENCES "user",
+  "questionnaire_type" VARCHAR(80) NOT NULL,
+  "score" INTEGER NOT NULL,
+  "note" TEXT,
+  "createdate" DATE NOT NULL
+);
+
+CREATE TABLE "resources" (
+  "id" SERIAL PRIMARY KEY,
+  "resource_type" VARCHAR(80) NOT NULL,
+  "resource_link" TEXT NOT NULL,
+  "resource_description" TEXT NOT NULL,
+  "createdate" DATE NOT NULL
 );
