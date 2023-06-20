af4ff9faec5f2f6c11fd98d02f564331ad4e7813
User <user@Users-MacBook-Pro.local>
2023-06-20 15:41:40 -0500

    created results saga/reducer files and had the response reducer setup for each

---
+++ src/redux/reducers/results.reducer.js
@@ -0,0 +1,28 @@
+const depressionResponseReducer = (state = {}, action) => {
+    switch (action.type) {
+      case 'SET_DEPRESSION_RESPONSE':
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+  
+  const anxietyResponseReducer = (state = {}, action) => {
+    switch (action.type) {
+      case 'SET_ANXIETY_RESPONSE':
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+  
+  const stressResponseReducer = (state = {}, action) => {
+    switch (action.type) {
+      case 'SET_STRESS_RESPONSE':
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+  
+  export { depressionResponseReducer, anxietyResponseReducer, stressResponseReducer };
