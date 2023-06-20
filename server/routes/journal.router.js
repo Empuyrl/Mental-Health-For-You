4dc93c499bad0431845fe07cbfb1d50f5992008b
User <user@Users-MacBook-Pro.local>
2023-06-20 13:06:55 -0500

    journal post works with and without user/category

--- server/routes/journal.router.js
+++ server/routes/journal.router.js
@@ -18,5 +18,26 @@ router.get('/entries', (req, res) => {
     });
 });
 
+// POST route to create a new journal entry
+router.post('/entries', (req, res) => {
+    // Extract data from the request body
+    //text for the entry and category for where it's held with the date
+    const { entry_text, category } = req.body;
+    const createdate = new Date();
+  
+    // Perform a database query to insert a new journal entry
+    const queryText = 'INSERT INTO entries (entry_text, createdate, category) VALUES ($1, $2, $3)';
+    const values = [entry_text, createdate, category];
+    pool
+      .query(queryText, values)
+      .then(() => {
+        res.sendStatus(201);
+      })
+      .catch((error) => {
+        console.error('Error creating journal entry:', error);
+        res.sendStatus(500);
+      });
+  });
+
 
 module.exports = router;
