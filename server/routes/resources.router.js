9dfb1b75c046d52370937c33aae3546ffec36513
User <user@Users-MacBook-Pro.local>
2023-06-20 14:47:07 -0500

    resource router done, ignore until base is over

--- server/routes/resources.router.js
+++ server/routes/resources.router.js
@@ -37,5 +37,29 @@ router.post('/', (req, res) => {
       });
   });
 
+  // Route to delete a resource
+router.delete('/:id', (req, res) => {
+    // Only allow logged-in users to delete resources
+    if (!req.user) {
+      return res.sendStatus(401); // Unauthorized
+    }
+  
+    const resourceId = req.params.id;
+    const user_id = req.user.id; // Retrieve the user ID from the authenticated user
+    const queryText = 'DELETE FROM resources WHERE id = $1 AND user_id = $2';
+    const values = [resourceId, user_id];
+    pool.query(queryText, values)
+      .then((result) => {
+        if (result.rowCount === 0) {
+          return res.sendStatus(404); // Resource not found or user is not authorized
+        }
+        res.sendStatus(200);
+      })
+      .catch((error) => {
+        console.error('Error deleting resource:', error);
+        res.sendStatus(500);
+      });
+  });
+
 
 module.exports = router;
