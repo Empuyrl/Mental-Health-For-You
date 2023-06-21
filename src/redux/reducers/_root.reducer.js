607c49214184c32db6b2ed80989077295dc92481
User <user@Users-MacBook-Pro.local>
2023-06-21 09:04:41 -0500

    reducers for journal and resources, think that's all

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -6,6 +6,8 @@ import {
   anxietyResponseReducer,
   stressResponseReducer
 } from './results.reducer';
+import journalReducer from './journal.reducer';
+import resourcesReducer from './resources.reducer';
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
 // This is imported in index.js as rootSaga
@@ -17,7 +19,9 @@ const rootReducer = combineReducers({
   user,
   depressionResponse: depressionResponseReducer,
   anxietyResponse: anxietyResponseReducer,
-  stressResponse: stressResponseReducer
+  stressResponse: stressResponseReducer,
+  journal: journalReducer,
+  resources: resourcesReducer,
 });
 
 
---
+++ src/redux/reducers/resources.reducer.js
@@ -0,0 +1,10 @@
+const resourcesReducer = (state = [], action) => {
+    switch (action.type) {
+      case 'SET_RESOURCES':
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+  
+  export default resourcesReducer;
