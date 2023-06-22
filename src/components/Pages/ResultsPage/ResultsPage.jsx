cee2685dc72fc00a6f600ff31c2917bfc7e6b3d7
User <user@Users-MacBook-Pro.local>
2023-06-22 13:27:54 -0500

    changed the results component a bit added dispatches and realized I was missing a router so going back to do that

--- src/components/Pages/ResultsPage/ResultsPage.jsx
+++ src/components/Pages/ResultsPage/ResultsPage.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Bar } from 'react-chartjs-2';
 
 function Results() {
+  const dispatch = useDispatch();
+
   // Retrieve the test results from the Redux store
   const depressionResult = useSelector((state) => state.depressionResult);
   const anxietyResult = useSelector((state) => state.anxietyResult);
@@ -11,51 +13,51 @@ function Results() {
 
   useEffect(() => {
     // Fetch the test results from the server upon component mount
-    // You'll need to dispatch the appropriate actions to retrieve the results
-    // e.g., dispatch({ type: 'FETCH_DEPRESSION_RESULT' });
-    // Make sure to handle any necessary loading and error states
-  }, []);
- // Calculate the severity level based on the result range
- const calculateSeverityLevel = (result, type) => {
-  if (type === 'depression') {
-    if (result >= 1 && result <= 4) {
-      return 'Minimal depression';
-    } else if (result >= 5 && result <= 9) {
-      return 'Mild depression';
-    } else if (result >= 10 && result <= 14) {
-      return 'Moderate depression';
-    } else if (result >= 15 && result <= 19) {
-      return 'Moderately severe depression';
-    } else if (result >= 20 && result <= 27) {
-      return 'Severe depression';
-    } else {
-      return 'Invalid Result';
-    }
-  } else if (type === 'anxiety') {
-    if (result >= 0 && result <= 4) {
-      return 'Minimal anxiety';
-    } else if (result >= 5 && result <= 9) {
-      return 'Mild anxiety';
-    } else if (result >= 10 && result <= 14) {
-      return 'Moderate anxiety';
-    } else if (result >= 15 && result <= 21) {
-      return 'Severe anxiety';
-    } else {
-      return 'Invalid Result';
-    }
-  } else if (type === 'stress') {
-    if (result >= 0 && result <= 13) {
-      return 'Low stress';
-    } else if (result >= 14 && result <= 26) {
-      return 'Moderate stress';
-    } else if (result >= 27 && result <= 40) {
-      return 'High perceived stress';
-    } else {
-      return 'Invalid Result';
-    }
-  }
-};
+    dispatch({ type: 'FETCH_DEPRESSION_RESPONSE' });
+    dispatch({ type: 'FETCH_ANXIETY_RESPONSE' });
+    dispatch({ type: 'FETCH_STRESS_RESPONSE' });
+  }, [dispatch]);
 
+  // Calculate the severity level based on the result range
+  const calculateSeverityLevel = (result, type) => {
+    if (type === 'depression') {
+      if (result >= 1 && result <= 4) {
+        return 'Minimal depression';
+      } else if (result >= 5 && result <= 9) {
+        return 'Mild depression';
+      } else if (result >= 10 && result <= 14) {
+        return 'Moderate depression';
+      } else if (result >= 15 && result <= 19) {
+        return 'Moderately severe depression';
+      } else if (result >= 20 && result <= 27) {
+        return 'Severe depression';
+      } else {
+        return 'Invalid Result';
+      }
+    } else if (type === 'anxiety') {
+      if (result >= 0 && result <= 4) {
+        return 'Minimal anxiety';
+      } else if (result >= 5 && result <= 9) {
+        return 'Mild anxiety';
+      } else if (result >= 10 && result <= 14) {
+        return 'Moderate anxiety';
+      } else if (result >= 15 && result <= 21) {
+        return 'Severe anxiety';
+      } else {
+        return 'Invalid Result';
+      }
+    } else if (type === 'stress') {
+      if (result >= 0 && result <= 13) {
+        return 'Low stress';
+      } else if (result >= 14 && result <= 26) {
+        return 'Moderate stress';
+      } else if (result >= 27 && result <= 40) {
+        return 'High perceived stress';
+      } else {
+        return 'Invalid Result';
+      }
+    }
+  };
 
   // Chart data and options for each questionnaire
   const depressionChartData = {
@@ -106,13 +108,13 @@ function Results() {
 
       {/* Display the test results */}
       <h3>Depression Result: {depressionResult}</h3>
-      <h4>Severity Level: {calculateSeverityLevel(depressionResult)}</h4>
+      <h4>Severity Level: {calculateSeverityLevel(depressionResult, 'depression')}</h4>
 
       <h3>Anxiety Result: {anxietyResult}</h3>
-      <h4>Severity Level: {calculateSeverityLevel(anxietyResult)}</h4>
+      <h4>Severity Level: {calculateSeverityLevel(anxietyResult, 'anxiety')}</h4>
 
       <h3>Stress Result: {stressResult}</h3>
-      <h4>Severity Level: {calculateSeverityLevel(stressResult)}</h4>
+      <h4>Severity Level: {calculateSeverityLevel(stressResult, 'stress')}</h4>
 
       {/* Link to the user's profile */}
       <Link to="/profile">View Profile</Link>
