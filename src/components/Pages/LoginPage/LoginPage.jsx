693eca422dd6acd8b6ec1b00a3e2756ec8fb5fe6
User <user@Users-MBP.hitronhub.home>
2023-06-30 06:51:34 -0500

    register and login page styling, image and link to one another

--- src/components/Pages/LoginPage/LoginPage.jsx
+++ src/components/Pages/LoginPage/LoginPage.jsx
@@ -1,26 +1,73 @@
 import React from 'react';
 import LoginForm from './LoginForm';
 import { useHistory } from 'react-router-dom';
+import { Box, Button, Typography } from '@mui/material';
+import { styled } from '@mui/system';
+
+const Container = styled(Box)(({ theme }) => ({
+  background: `url(https://hbr.org/resources/images/article_assets/2018/10/Oct22_18_862457080.png)`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  minHeight: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+}));
+
+const FormContainer = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(4),
+  backgroundColor: 'white',
+  borderRadius: theme.spacing(2),
+  marginBottom: theme.spacing(2),
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+}));
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  backgroundColor: '#007bff',
+  color: 'white',
+  fontWeight: 'bold',
+  borderRadius: theme.spacing(2),
+  padding: theme.spacing(1, 3),
+  marginTop: theme.spacing(2),
+  cursor: 'pointer',
+  transition: 'transform 0.3s, background-color 0.3s',
+  '&:hover': {
+    backgroundColor: '#0056b3',
+    transform: 'translateY(-2px)',
+  },
+}));
+
+const StyledLink = styled(Typography)(({ theme }) => ({
+  color: '#00FF00',
+  fontWeight: 'bold',
+  marginTop: theme.spacing(2),
+  cursor: 'pointer',
+  transition: 'transform 0.3s, color 0.3s',
+  '&:hover': {
+    color: '#00CC00',
+    transform: 'translateY(-2px)',
+  },
+}));
 
 function LoginPage() {
   const history = useHistory();
 
-  return (
-    <div>
-      <LoginForm />
+  const handleRegisterClick = () => {
+    history.push('/register');
+  };
 
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
-          Register
-        </button>
-      </center>
-    </div>
+  return (
+    <Container>
+      <FormContainer>
+        <LoginForm />
+      </FormContainer>
+
+      <StyledLink variant="subtitle1" onClick={handleRegisterClick}>
+        Don't have an account? Register here.
+      </StyledLink>
+    </Container>
   );
 }
 
--- src/components/Pages/RegisterPage/RegisterPage.jsx
+++ src/components/Pages/RegisterPage/RegisterPage.jsx
@@ -1,39 +1,73 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import RegisterForm from './RegisterForm';
-import { Box } from '@mui/material';
+import { useHistory } from 'react-router-dom';
+import { Box, Button, Typography } from '@mui/material';
 import { styled } from '@mui/system';
 
 const Container = styled(Box)(({ theme }) => ({
-  backgroundImage: 'url("background_image.jpg")',
+  background: `url(https://hbr.org/resources/images/article_assets/2018/10/Oct22_18_862457080.png)`,
   backgroundSize: 'cover',
   backgroundPosition: 'center',
-  backgroundRepeat: 'no-repeat',
   minHeight: '100vh',
   display: 'flex',
-  justifyContent: 'center',
+  flexDirection: 'column',
   alignItems: 'center',
-  gradient: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+}));
+
+const FormContainer = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(4),
+  backgroundColor: 'white',
+  borderRadius: theme.spacing(2),
+  marginBottom: theme.spacing(2),
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+}));
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  backgroundColor: '#007bff',
+  color: 'white',
+  fontWeight: 'bold',
+  borderRadius: theme.spacing(2),
+  padding: theme.spacing(1, 3),
+  marginTop: theme.spacing(2),
+  cursor: 'pointer',
+  transition: 'transform 0.3s, background-color 0.3s',
+  '&:hover': {
+    backgroundColor: '#0056b3',
+    transform: 'translateY(-2px)',
+  },
+}));
+
+const StyledLink = styled(Typography)(({ theme }) => ({
+  color: '#00FF00',
+  fontWeight: 'bold',
+  marginTop: theme.spacing(2),
+  cursor: 'pointer',
+  transition: 'transform 0.3s, color 0.3s',
+  '&:hover': {
+    color: '#00CC00',
+    transform: 'translateY(-2px)',
+  },
 }));
 
 function RegisterPage() {
   const history = useHistory();
 
+  const handleLoginClick = () => {
+    history.push('/login');
+  };
+
   return (
     <Container>
-      <RegisterForm />
+      <FormContainer>
+        <RegisterForm />
 
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/login');
-          }}
-        >
-          Login
-        </button>
-      </center>
+      </FormContainer>
+
+      <StyledLink variant="subtitle1" onClick={handleLoginClick}>
+        Already have an account? Login here.
+      </StyledLink>
     </Container>
   );
 }
