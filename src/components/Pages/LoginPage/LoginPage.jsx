abda8de03a7f945369aac6c3d270d13addf1d1a6
User <user@Users-MBP.hitronhub.home>
2023-06-30 07:25:07 -0500

    changed a bit on the pages, have to figure out why that link isn't workin. login forms done, need to be moved down, could be a white space issue.

--- src/components/Pages/LoginPage/LoginPage.jsx
+++ src/components/Pages/LoginPage/LoginPage.jsx
@@ -24,21 +24,6 @@ const FormContainer = styled(Box)(({ theme }) => ({
   alignItems: 'center',
 }));
 
-const StyledButton = styled(Button)(({ theme }) => ({
-  backgroundColor: '#007bff',
-  color: 'white',
-  fontWeight: 'bold',
-  borderRadius: theme.spacing(2),
-  padding: theme.spacing(1, 3),
-  marginTop: theme.spacing(2),
-  cursor: 'pointer',
-  transition: 'transform 0.3s, background-color 0.3s',
-  '&:hover': {
-    backgroundColor: '#0056b3',
-    transform: 'translateY(-2px)',
-  },
-}));
-
 const StyledLink = styled(Typography)(({ theme }) => ({
   color: '#00FF00',
   fontWeight: 'bold',
--- src/components/Pages/RegisterPage/RegisterPage.jsx
+++ src/components/Pages/RegisterPage/RegisterPage.jsx
@@ -24,21 +24,6 @@ const FormContainer = styled(Box)(({ theme }) => ({
   alignItems: 'center',
 }));
 
-const StyledButton = styled(Button)(({ theme }) => ({
-  backgroundColor: '#007bff',
-  color: 'white',
-  fontWeight: 'bold',
-  borderRadius: theme.spacing(2),
-  padding: theme.spacing(1, 3),
-  marginTop: theme.spacing(2),
-  cursor: 'pointer',
-  transition: 'transform 0.3s, background-color 0.3s',
-  '&:hover': {
-    backgroundColor: '#0056b3',
-    transform: 'translateY(-2px)',
-  },
-}));
-
 const StyledLink = styled(Typography)(({ theme }) => ({
   color: '#00FF00',
   fontWeight: 'bold',
@@ -72,4 +57,4 @@ function RegisterPage() {
   );
 }
 
-export default RegisterPage;
+export default RegisterPage; 
